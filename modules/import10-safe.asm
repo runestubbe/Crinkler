f1130a58407e11465951a169130eb3f0436e2105
aske <aske@localhost>
2006-12-21 14:25:21 +0000

    new header, manual

--- modules/import10-safe.asm
+++ modules/import10-safe.asm
@@ -24,19 +24,19 @@ _Import:
 	mov		esi, _DLLNames
 	mov		edi, _ImportList
 
-	xor	eax, eax
-	mov	eax, [fs:eax+30h]		; PEB  base
-	mov	eax, [eax+0ch]		; goto PEB_LDR_DATA
+	xor		eax, eax
+	mov		eax, [fs:eax+30h]		; PEB  base
+	mov		eax, [eax+0ch]			; goto PEB_LDR_DATA
 	; first entry in InInitializationOrderModuleList
-	mov	eax, [eax+1ch]
-	mov	eax, [byte eax+00h]		; forward to next LIST_ENTRY
-	mov	ebp, [eax+08h]		; Kernel32 base memory
+	mov		eax, [eax+1ch]
+	mov		eax, [byte eax+00h]		; forward to next LIST_ENTRY
+	mov		ebp, [eax+08h]			; Kernel32 base memory
 
 ;	pop		ebp
-;	push		ebp
+;	push	ebp
 ;FindKernel32:
 ;	dec		ebp
-;	cmp		[ebp], dword 00905A4Dh		; DOS header 00905a4dh
+;	cmp		[ebp], dword 00905A4Dh	; DOS header 00905a4dh
 ;	jne		FindKernel32
 
 DLLLoop:
@@ -53,7 +53,7 @@ DLLLoop:
 
 	xor		eax, eax
 	lodsb
-	xchg		ecx, eax
+	xchg	ecx, eax
 HashLoop:
 	pusha
 
@@ -61,16 +61,16 @@ GetProcAddress:
 	mov		eax, [ebp + 3ch]		; eax = PE header offset
 	add		eax, ebp
 	mov		edx, [eax + 78h]		; edx = exports directory table offset
-	add		edx, ebp			; edx = exports directory table address
+	add		edx, ebp				; edx = exports directory table address
 	mov		ecx, [edx + 18h]		; ecx = number of names
 
 	; Check all names of procedures for the right hash
 
 ScanProcedureNamesLoop:
 	mov		eax, [edx + 20h]		; edx = name pointers table offset
-	add		eax, ebp			; edx = name pointers table address
-	mov		esi, [eax + ecx*4 - 4]		; esi = name pointer offset
-	add		esi, ebp			; esi = name pointer address
+	add		eax, ebp				; edx = name pointers table address
+	mov		esi, [eax + ecx*4 - 4]	; esi = name pointer offset
+	add		esi, ebp				; esi = name pointer address
 	xor		edi, edi
 
 CalculateHashLoop:
@@ -81,7 +81,7 @@ CalculateHashLoop:
 	dec		eax
 	jge		CalculateHashLoop
 
-	cmp		edi, [ebx]			; check computed hash
+	cmp		edi, [ebx]				; check computed hash
 	loopne	ScanProcedureNamesLoop
 
 ;	; For debugging
@@ -98,10 +98,10 @@ CalculateHashLoop:
 
 	; Found, get the address from the table
 	mov		eax, [edx + 24h]		; ebx = ordinals table RNA offset
-	add		eax, ebp			; ebx = ordinals table RNA address
+	add		eax, ebp				; ebx = ordinals table RNA address
 	mov		cx, [eax + ecx*2]		; ecx = function ordinal
 	mov		eax, [edx + 1ch]		; ebx = address table RVA offset
-	add		eax, ebp			; ebx = address table RVA address
+	add		eax, ebp				; ebx = address table RVA address
 	lea		eax, [eax + ecx*4]		; ebp = address of function RVA address
 
 	mov		[esp + 20], eax			; stack position of edx
