c2d99663a11751ec7e1ddbf59960ee5b3a1d1547
Aske Simon Christensen <askeksa@users.noreply.github.com>
2007-07-07 17:05:15 +0000

    New colors and disassembly for html output

--- HtmlSummary.cpp
+++ HtmlSummary.cpp
@@ -6,20 +6,73 @@
 #include "Compressor/Compressor.h"
 #include "Log.h"
 #include "Hunk.h"
+#include "Crinkler.h"
+#include "distorm.h"
 
 
 using namespace std;
 
+const int NUM_COLUMNS = 16;
+const int OPCODE_WIDTH = 11;
+
+struct scol {
+	float size;
+	unsigned color;
+} sizecols[] = {
+	{ 0.1f, 0x80ff80 },
+	{ 0.5f, 0x00ff00 },
+	{ 1.0f, 0x00a000 },
+	{ 2.0f, 0x008040 },
+	{ 3.0f, 0x006090 },
+	{ 5.0f, 0x0020f0 },
+	{ 7.0f, 0x0000a0 },
+	{ 9.0f, 0x000000 },
+	{ 12.0f, 0x900000 },
+};
+
 static int sizeToColor(int size) {
-	int v = min(size/16, 255);	//between 0 and 2 bytes
-	return (v<<16) | ((255-v)<<8);
+	float bsize = size / (float)BITPREC;
+	for (int i = 0 ; i < sizeof(sizecols)/sizeof(scol) ; i++) {
+		if (bsize < sizecols[i].size) {
+			return sizecols[i].color;
+		}
+	}
+	return 0xff0000;
+}
+
+static int instructionSize(_DecodedInst *inst, const int *sizefill) {
+	int idx = (int)inst->offset - CRINKLER_CODEBASE;
+	int size = sizefill[idx + inst->size] - sizefill[idx];
+	size /= inst->size;
+	return size;
 }
 
 static int toAscii(int c) {
 	return (c >= 32 && c <= 126 || c >= 160) ? c : '.';
 }
 
-void htmlSummaryRecursive(CompressionSummaryRecord* csr, FILE* out, const unsigned char* data, int datasize, const int* sizefill) {
+static void printRow(FILE *out, const unsigned char *data, const int *sizefill, int index, int n, int datasize, bool ascii) {
+	for(int x = 0; x < n; x++) {
+		unsigned char c = 0;
+		int size = 0;
+		int idx = index + x;
+		if(idx < datasize) {
+			size = sizefill[idx+1]-sizefill[idx];
+			c = data[idx++];
+		}
+
+		if (ascii) {
+			fprintf(out,"<td title=\"%.2f bits\" style=\"color: #%.6X;\">"
+				"%c</td>", size / (float)BITPREC, sizeToColor(size), toAscii(c));
+		} else {
+			fprintf(out,"<td title=\"%.2f bits\" style=\"color: #%.6X;\">"
+				"%.2X</td>", size / (float)BITPREC, sizeToColor(size), c);
+		}
+	}
+
+}
+
+void htmlSummaryRecursive(CompressionSummaryRecord* csr, FILE* out, const unsigned char* data, int datasize, const int* sizefill, bool iscode) {
 	//handle enter node events
 	if(csr->type & RECORD_ROOT) {
 		//write header
@@ -58,95 +111,166 @@ void htmlSummaryRecursive(CompressionSummaryRecord* csr, FILE* out, const unsign
 				"<h1>%s</h1>"
 				"<table class=\"outertable\">"
 				"<tr>"
+				"<th>address</th>"
 				"<th>label name</th>"
-				"<th>pos</th>"
-				"<th>comp-pos</th>"
-				"<th>size</th>"
-				"<th>compsize</th>"
-				"</tr>", csr->name.c_str());
-		} else {
-			if(csr->type & RECORD_PUBLIC)
-				fprintf(out, "<tr><td><b>%s</b></td>", csr->name.c_str());	//emphasize global labels
-			else
-				fprintf(out, "<tr><td>%s</td>", csr->name.c_str());
+				"<th>size</th>",
+				csr->name.c_str());
 
-			if(csr->compressedPos >= 0) {
-				fprintf(out,"<td align=right>%9d</td>"
-					"<td align=right>%.2f</td>"
-					"<td align=right>%d</td>"
-					"<td align=right>%.2f</td>",
-					csr->pos, csr->compressedPos / (BITPREC*8.0f), csr->size, csr->compressedSize / (BITPREC*8.0f));
+			if (csr->name.c_str()[0] == 'U') {
+				fprintf(out, "</tr>");
 			} else {
-				fprintf(out,"<td align=right>%9d</td>"
-					"<td/>"
-					"<td align=right>%d</td>"
-					"<td/>",
-					csr->pos, csr->size);
+				fprintf(out,
+					"<th>comp. size</th>"
+					"<th>ratio</th>"
+					"</tr>");
+			}
+			if (csr->name.c_str()[0] == 'C') {
+				iscode = true;
+			}
+		} else {
+
+			fprintf(out, "<tr><td><table class=\"data\"><tr><td class=\"address\">%.8X&nbsp;</td></tr></table></td>",
+					CRINKLER_CODEBASE + csr->pos);
+
+			if(csr->type & RECORD_PUBLIC)
+				fprintf(out, "<td><b>%s</b></td>", csr->name.c_str());	//emphasize global labels
+			else
+				fprintf(out, "<td>%s</td>", csr->name.c_str());
+
+			if (csr->size > 0) {
+				if(csr->compressedPos >= 0) {
+					fprintf(out,
+						"<td align=right>%d</td>"
+						"<td align=right>%.2f</td>"
+						"<td align=right>%.1f%%</td>",
+						csr->size,
+						csr->compressedSize / (BITPREC*8.0f),
+						(csr->compressedSize / (BITPREC*8.0f)) * 100 / csr->size);
+				} else {
+					fprintf(out,
+						"<td align=right>%d</td>",
+						csr->size);
+				}
+			} else {
+				fprintf(out, "<td/><td/><td/>");
 			}
 			fprintf(out,"</tr>");
 
 			if(csr->compressedPos >= 0 && csr->size > 0) {
-				const int NUM_COLUMNS = 32;
-				
-				//write hex
-				{
-					int bytesleft = csr->size;
+				if (iscode) {
+					// Code section
+
+					// Allocate space for worst-case number of instructions
+					int instspace = csr->size;
+					if (instspace < 15) instspace = 15;
+					_DecodedInst *insts = (_DecodedInst *)malloc(instspace * sizeof(_DecodedInst));
+					unsigned int numinsts;
+					distorm_decode(CRINKLER_CODEBASE + csr->pos, &data[csr->pos], csr->size, Decode32Bits, insts, instspace, &numinsts);
+
+					// Print instruction adresses
 					fprintf(out, "<tr><td><table class=\"data\">");
-					int idx = csr->pos;
-					while(bytesleft > 0) {
-						int ncolumns = min(bytesleft, NUM_COLUMNS);
+					for (int i = 0 ; i < numinsts ; i++) {
 						fprintf(out, "<tr>");
-						fprintf(out, "<td class=\"address\">%.4x</td>", idx);
-
-						for(int x = 0; x < ncolumns; x++) {
-							unsigned char c = 0;
-							int size = 0;
-							if(idx < datasize) {
-								size = sizefill[idx+1]-sizefill[idx];
-								c = data[idx++];
-							}
-
-							fprintf(out,"<td title=\"%.2f bits\" style=\"color: #%.6X;\">"
-								"%.2X</td>", size / (float)BITPREC, sizeToColor(size), c);
-						}
+						fprintf(out, "<td class=\"address\">%.8X&nbsp;</td>", insts[i].offset);
 						fprintf(out, "</tr>");
-						bytesleft -= ncolumns;
 					}
 					fprintf(out, "</table></td>");
-				}
-				
 
-				//write ascii
-				{
-					int bytesleft = csr->size;
-					fprintf(out, "<td colspan=4><table class=\"data\">");
-					int idx = csr->pos;
-					while(bytesleft > 0) {
-						int ncolumns = min(bytesleft, NUM_COLUMNS);
+					// Print hex dump of instructions
+					fprintf(out, "<td><table class=\"data\">");
+					for (int i = 0 ; i < numinsts ; i++) {
 						fprintf(out, "<tr>");
-
-						for(int x = 0; x < ncolumns; x++) {
-							unsigned char c = 0;
-							int size = 0;
-							if(idx < datasize) {
-								size = sizefill[idx+1]-sizefill[idx];
-								c = data[idx++];
-							}
-
-							fprintf(out,"<td title=\"%.2f bits\" style=\"color: #%.6X;\">"
-								"%c</td>", size / (float)BITPREC, sizeToColor(size), toAscii(c));
-						}
+						printRow(out, data, sizefill, (int)insts[i].offset - CRINKLER_CODEBASE, insts[i].size, datasize, false);
 						fprintf(out, "</tr>");
-						bytesleft -= ncolumns;
+					}
+					fprintf(out, "</table></td>");
+
+					// Print instructions
+					fprintf(out, "<td colspan=3><table class=\"data\">");
+					for (int i = 0 ; i < numinsts ; i++) {
+						// Make hex digits uppercase
+						for (unsigned char *cp = insts[i].operands.p ; *cp != 0 ; cp++) {
+							if (*cp >= 'a' && *cp <= 'f') *cp += 'A'-'a';
+						}
+						int size = instructionSize(&insts[i], sizefill);
+
+						fprintf(out, "<tr><td title=\"%.2f bits/byte\" style=\"color: #%.6X;\">%s",
+							size / (float)BITPREC, sizeToColor(size), insts[i].mnemonic.p);
+						for (int j = 0 ; j < OPCODE_WIDTH-insts[i].mnemonic.length ; j++) {
+							fprintf(out, "&nbsp;");
+						}
+						fprintf(out, "&nbsp;%s</td></tr>", insts[i].operands.p);
 					}
 					fprintf(out, "</table></td></tr>");
+
+					free(insts);
+
+				} else {
+					// Data section
+
+					// Print adresses
+					{
+						fprintf(out, "<tr><td><table class=\"data\">");
+						int bytesleft = csr->size;
+						int idx = csr->pos;
+						while(bytesleft > 0) {
+							int ncolumns = min(bytesleft, NUM_COLUMNS);
+
+							fprintf(out, "<tr>");
+							fprintf(out, "<td class=\"address\">%.8X&nbsp;</td>", CRINKLER_CODEBASE + idx);
+							fprintf(out, "</tr>");
+
+							bytesleft -= ncolumns;
+							idx += ncolumns;	
+						}
+						fprintf(out, "</table></td>");
+					}
+
+					//write hex
+					{
+						fprintf(out, "<td><table class=\"data\">");
+						int bytesleft = csr->size;
+						int idx = csr->pos;
+						while(bytesleft > 0) {
+							int ncolumns = min(bytesleft, NUM_COLUMNS);
+
+							fprintf(out, "<tr>");
+							printRow(out, data, sizefill, idx, ncolumns, datasize, false);
+							fprintf(out, "</tr>");
+
+							bytesleft -= ncolumns;
+							idx += ncolumns;	
+						}
+						fprintf(out, "</table></td>");
+					}
+					
+
+					//write ascii
+					{
+						fprintf(out, "<td colspan=3><table class=\"data\">");
+						int bytesleft = csr->size;
+						int idx = csr->pos;
+						while(bytesleft > 0) {
+							int ncolumns = min(bytesleft, NUM_COLUMNS);
+
+							fprintf(out, "<tr>");
+							printRow(out, data, sizefill, idx, ncolumns, datasize, true);
+							fprintf(out, "</tr>");
+
+							bytesleft -= ncolumns;
+							idx += ncolumns;	
+						}
+						fprintf(out, "</table></td></tr>");
+					}
+					
 				}
+
 			}
 		}
 	}
 
 	for(vector<CompressionSummaryRecord*>::iterator it = csr->children.begin(); it != csr->children.end(); it++)
-		htmlSummaryRecursive(*it, out, data, datasize, sizefill);
+		htmlSummaryRecursive(*it, out, data, datasize, sizefill, iscode);
 
 	//handle leave node event
 	if(csr->type == RECORD_ROOT) {
@@ -165,7 +289,7 @@ void htmlSummary(CompressionSummaryRecord* csr, const char* filename, const unsi
 		Log::error(0, "", "could not open '%s' for writing", filename);
 		return;
 	}
-	htmlSummaryRecursive(csr, out, data, datasize, sizefill);
+	htmlSummaryRecursive(csr, out, data, datasize, sizefill, false);
 
 	fclose(out);
 }
