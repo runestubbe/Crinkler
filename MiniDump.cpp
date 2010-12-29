401780663c1fe4350d0eddecf66c138d01c9fdda
Rune Stubbe <22775506+runestubbe@users.noreply.github.com>
2010-12-29 14:00:50 +0000

    minidumps include a mini and a full dump

--- MiniDump.cpp
+++ MiniDump.cpp
@@ -8,24 +8,31 @@ LONG WINAPI ExitWithDump(struct _EXCEPTION_POINTERS* exceptionInfo)
 {
 	int dump_index = 0;
 
-	HANDLE file = NULL;
+	HANDLE file_mini = NULL;
+	HANDLE file_full = NULL;
+	char filename_mini[128];
+	char filename_full[128];
 	do {
-		char buff[128];
-		sprintf(buff, "dump%04d.dmp", dump_index);
+		sprintf(filename_mini, "dump%04d_mini.dmp", dump_index);
+		sprintf(filename_full, "dump%04d_full.dmp", dump_index);
 		dump_index++;
 
-		file = CreateFile(buff, GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	} while(file == NULL || file == INVALID_HANDLE_VALUE);
+		file_mini = CreateFile(filename_mini, GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
+		file_full = CreateFile(filename_full, GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
+	} while(file_mini == NULL || file_mini == INVALID_HANDLE_VALUE || file_full == NULL || file_full == INVALID_HANDLE_VALUE);
 
 	MINIDUMP_EXCEPTION_INFORMATION mdei; 
 	mdei.ThreadId           = GetCurrentThreadId(); 
 	mdei.ExceptionPointers  = exceptionInfo; 
 	mdei.ClientPointers     = FALSE;
-	MINIDUMP_TYPE mdt       = MiniDumpNormal;
 
-	MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), file, mdt, (exceptionInfo != 0) ? &mdei : 0, 0, 0); 
-	CloseHandle(file);
+	MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), file_mini, MiniDumpNormal, (exceptionInfo != 0) ? &mdei : 0, 0, 0); 
+	MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), file_full, MiniDumpWithFullMemory, (exceptionInfo != 0) ? &mdei : 0, 0, 0); 
+	CloseHandle(file_mini);
+	CloseHandle(file_full);
 
+	printf("\nOops! Crinkler has crashed.\nDump files written to %s and %s.\n", filename_mini, filename_full);
+	ExitProcess(-1);
 	return EXCEPTION_CONTINUE_SEARCH;
 }
 
