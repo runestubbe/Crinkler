43f606f6d21d4559fc402e337625dfc4602c84ae
runelhs <runelhs@localhost>
2015-07-28 00:28:45 +0200

    hack to get continuation to import hunk

--- Transform.cpp
+++ Transform.cpp
@@ -7,19 +7,22 @@
 
 bool Transform::linkAndTransform(HunkList* hunklist, Symbol *entry_label, int baseAddress, Hunk* &transformedHunk, Hunk** untransformedHunk, int* splittingPoint, bool verbose)
 {
-	Hunk* detrans;
+	Hunk* detrans = nullptr;
 	if (m_enabled)
 	{
 		detrans = getDetransformer();
-
-		if (!detrans)
+		if(detrans)
 		{
-			detrans = new Hunk("Stub", NULL, HUNK_IS_CODE, 0, 0, 0);
+			detrans->setVirtualSize(detrans->getRawSize());
 		}
-		detrans->setVirtualSize(detrans->getRawSize());
-		hunklist->addHunkFront(detrans);
-		detrans->setContinuation(entry_label);
 	}
+
+	if(!detrans)
+	{
+		detrans = new Hunk("Stub", NULL, HUNK_IS_CODE, 0, 0, 0);
+	}
+	hunklist->addHunkFront(detrans);
+	detrans->setContinuation(entry_label);
 
 	int sp;
 	transformedHunk = hunklist->toHunk("linked", baseAddress, &sp);
@@ -35,11 +38,11 @@ bool Transform::linkAndTransform(HunkList* hunklist, Symbol *entry_label, int ba
 		*untransformedHunk = new Hunk(*transformedHunk);
 	}
 
+	hunklist->removeHunk(detrans);
+	delete detrans;
+
 	if (m_enabled)
 	{
-		hunklist->removeHunk(detrans);
-		delete detrans;
-
 		return transform(transformedHunk, sp, verbose);
 	}
 	else
