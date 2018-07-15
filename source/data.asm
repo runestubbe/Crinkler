93c62f3e723ae88eff02b6f0786a9204020de174
Rune Stubbe <22775506+runestubbe@users.noreply.github.com>
2018-07-15 23:56:34 +0200

    Removed modules project and migrated files to Crinkler project. Module obj files are now correctly written to the artifacts folder

--- source/data.asm
+++ source/data.asm
@@ -27,52 +27,58 @@ global _knownDllExports_end
 
 section	.data
 
+%macro INCBIN 1
+%defstr T PATH
+%strcat S T, %1
+incbin S
+%endmacro
+
 _header11Obj:
-	incbin "modules/header11.obj"
+	INCBIN "header11.obj"
 _header11Obj_end:
 
 _headerObj:
-	incbin "modules/header20.obj"
+	INCBIN "header20.obj"
 _headerObj_end:
 
 _headerCompatibilityObj:
-	incbin "modules/header20_compatibility.obj"
+	INCBIN "header20_compatibility.obj"
 _headerCompatibilityObj_end:
 
 _header1KObj:
-	incbin "modules/header20_1k.obj"
+	INCBIN "header20_1k.obj"
 _header1KObj_end:
 
 _import1KObj:
-	incbin "modules/import20_1k.obj"
+	INCBIN "import20_1k.obj"
 _import1KObj_end:
 
 _importObj:
-	incbin "modules/import20.obj"
+	INCBIN "import20.obj"
 _importObj_end:
 
 _importRangeObj:
-	incbin "modules/import20-range.obj"
+	INCBIN "import20-range.obj"
 _importRangeObj_end:
 
 _importSafeObj:
-	incbin "modules/import20-safe.obj"
+	INCBIN "import20-safe.obj"
 _importSafeObj_end:
 
 _importSafeRangeObj:
-	incbin "modules/import20-safe-range.obj"
+	INCBIN "import20-safe-range.obj"
 _importSafeRangeObj_end:
 
 _importSafeFallbackObj:
-	incbin "modules/import20-safe-fallback.obj"
+	INCBIN "import20-safe-fallback.obj"
 _importSafeFallbackObj_end:
 
 _importSafeFallbackRangeObj:
-	incbin "modules/import20-safe-fallback-range.obj"
+	INCBIN "import20-safe-fallback-range.obj"
 _importSafeFallbackRangeObj_end:
 
 _calltransObj:
-	incbin "modules/calltrans.obj"
+	INCBIN "calltrans.obj"
 _calltransObj_end:
 
 _knownDllExports:
