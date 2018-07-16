8f32d0d278e7df3445c7fc1ffa16dc0113711d58
Rune Stubbe <22775506+runestubbe@users.noreply.github.com>
2018-07-16 20:47:54 +0200

    Ported ModelHash to C++. Fixed data.asm and log_table.asm for x64.

---
+++ source/Compressor/Model.cpp
@@ -0,0 +1,44 @@
+#include "model.h"
+
+unsigned int __stdcall ModelHashStart(unsigned int mask, int hashmul)
+{
+	unsigned char dl = mask;
+	unsigned int hash = mask;
+
+	hash = hash * hashmul - 1;
+
+	while(dl)
+	{
+		if((dl & 0x80))
+			hash = hash * hashmul - 1;
+		dl += dl;
+	}
+	return hash;
+}
+
+unsigned int __stdcall ModelHash(const byte *data, int bitpos, unsigned int mask, int hashmul)
+{
+	unsigned char dl = mask;
+	const byte* ptr = data + (bitpos >> 3);
+	
+	unsigned int hash = mask;
+	byte current_byte = (0x100 | *ptr) >> ((~bitpos & 7) + 1);
+	hash ^= current_byte;
+	hash *= hashmul;
+	hash = (hash & 0xFFFFFF00) | ((hash + current_byte) & 0xFF);
+	hash--;
+	while(dl != 0)
+	{
+		ptr--;
+		if(dl & 0x80)
+		{
+			current_byte = *ptr;
+			hash ^= current_byte;
+			hash *= hashmul;
+			hash = (hash & 0xFFFFFF00) | ((hash + current_byte) & 0xFF);
+			hash--;
+		}
+		dl += dl;
+	}
+	return hash;
+}
--- source/Compressor/model.h
+++ source/Compressor/model.h
@@ -6,6 +6,6 @@ extern "C" {
 	inline int __stdcall GetBit(const byte *data, int bitpos) {
 		return (data[bitpos >> 3] >> (7 - bitpos & 7)) & 1;
 	}
-	unsigned int __stdcall ModelHash(const byte *data, int bitpos, unsigned int mask, int hashmul);
 	unsigned int __stdcall ModelHashStart(unsigned int mask, int hashmul);
+	unsigned int __stdcall ModelHash(const byte *data, int bitpos, unsigned int mask, int hashmul);
 };
