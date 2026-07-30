593ef58e06ebdae4400dbe55b60eb0fadc8c8fba
Aske Simon Christensen <askesc@gmail.com>
2026-07-30 09:38:48 +0200

    Version and manual update for 3.0b

--- source/Crinkler/Crinkler.h
+++ source/Crinkler/Crinkler.h
@@ -37,8 +37,8 @@ static const int PRINT_LABELS =		1;
 static const int PRINT_IMPORTS =	2;
 static const int PRINT_MODELS =		4;
 
-#define CRINKLER_TITLE "Crinkler 3.0a (" __DATE__ ") (c) 2005-2026 Aske Simon Christensen & Rune Stubbe"
-#define CRINKLER_WITH_VERSION "Crinkler 3.0a"
+#define CRINKLER_TITLE "Crinkler 3.0b (" __DATE__ ") (c) 2005-2026 Aske Simon Christensen & Rune Stubbe"
+#define CRINKLER_WITH_VERSION "Crinkler 3.0b"
 static const int CRINKLER_LINKER_VERSION = 0x3033;
 
 class Crinkler {
